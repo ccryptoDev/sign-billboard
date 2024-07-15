627c77bf43807ecf68b4009899c345573444066f
ccryptoDev <smartwebdev90@gmail.com>
2024-07-15 16:17:07 +0200

    removed refresh token log

--- app/Console/Commands/Refresh_token.php
+++ app/Console/Commands/Refresh_token.php
@@ -62,7 +62,7 @@ class Refresh_token extends Command
         curl_close($curl);
         $apiToken = json_decode($response,true)["apiToken"];
         
-        \Illuminate\Support\Facades\Log::info("artisan command: refresh:token: updated token: " . $apiToken);
+        // \Illuminate\Support\Facades\Log::info("artisan command: refresh:token: updated token: " . $apiToken);
 
         $token = CMToken::first();
         if(isset($token->id)){
