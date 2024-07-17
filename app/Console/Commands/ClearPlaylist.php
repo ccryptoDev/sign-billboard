24f5bfe3a16bb42f30604a023b9fb716e0853a65
ccryptoDev <smartwebdev90@gmail.com>
2024-07-17 10:07:22 +0200

    removed the log of clear list

--- app/Console/Commands/ClearPlaylist.php
+++ app/Console/Commands/ClearPlaylist.php
@@ -38,7 +38,7 @@ class ClearPlaylist extends Command
      */
     public function handle()
     {
-        \Illuminate\Support\Facades\Log::info("artisan command: clear:list");
+        // \Illuminate\Support\Facades\Log::info("artisan command: clear:list");
 
         $controller = app()->make('App\Http\Controllers\MainController');
         $apiToken = app()->call([$controller, 'get_token']);
