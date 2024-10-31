fafe943c0eba30996940d7a79266887014b45216
ccryptoDev <smartwebdev90@gmail.com>
2024-10-31 21:50:10 +0100

    new verification code

--- resources/views/front/header-copy.blade.php
+++ resources/views/front/header-copy.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="google-site-verification" content="ZiA6vbw8FwvzCNV_KHqNdYMMhmfncMvUvO7d6HsK3o"/>
+        <meta name="google-site-verification" content="ZtxW9imGT_Mew1mRmK0J0v0NksxA6fQdqG2K5a82x20"/>
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         
         <?php
